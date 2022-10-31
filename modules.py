13f55c1f5a1cf88c025ef7e11d298b233eb77a0a
FourHole <fholifi1@gmail.com>
2022-10-30 20:30:20 -0500

    updating

--- modules.py
+++ modules.py
@@ -1,18 +1,42 @@
-class create_vapp_vm:
-    def __init__(self, name, os, ram, cpu):
-        self.name = name
-        self.os = os
-        self.ram = ram
-        self.cpu = cpu
+import json
+import os
 
-    def __str__(self):
-        return (
-            f'resource "vcd_vapp_vm" "{self.name}" {{\n' 
-            f'  name    = "{self.name}"\n' 
-            f'  os      = "{self.os}"\n' 
-            f'  memory  = {self.ram}\n' 
-            f'  cpus    = {self.cpu}\n' 
-            f'}}'
-        )
+class file_check:
+    def __init__(self, file_name):
+        
+        if not os.path.exists(file_name):
+            open(file_name, 'a').close
 
+        if os.stat(file_name).st_size == 0:
+            out_data = {
+                "resource": {
+                    "aws_instance": {
 
+                    }
+                }
+            }
+            with open(file_name, 'w') as js:
+                json.dump(out_data, js, indent=4)
+            js.close()
+
+class create_vms:
+    def __init__(self, vm_list, vm_config_file):
+        
+        with open(vm_config_file) as js:
+            data = json.load(js)
+
+        for row in vm_list.itertuples(index=True, name='Pandas'):
+
+            data['resource']['aws_instance'][row.name] = {
+                "ami": row.ami,
+                "instance_type": row.instance_type,
+                "key_name": row.key_name,
+                "Tags": {
+                    "Name": row.name
+            }
+        }
+
+        with open(vm_config_file, 'w') as js:
+            json.dump(data, js, indent=4)
+
+        js.close()
